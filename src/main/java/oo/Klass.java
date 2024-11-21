21d781feee4bb304a7b781666a43e610fbfc3172
Emily Ng <ngem3@oocl.com>
2024-11-21 14:37:05 +0800

    feat: Step3

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,4 +1,24 @@
 package oo;
 
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+    public int getClassNumber() {
+        return this.number;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Klass anotherClass = (Klass) obj;
+        return this.number == anotherClass.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.number;
+    }
 }
--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -2,13 +2,23 @@ package oo;
 
 public class Student extends Person{
     protected static final String OCCUPATION = "student";
+    private Klass klass;
 
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
+    public void join(Klass klass) {
+        this.klass = klass;
+    }
+
+    public boolean isIn(Klass klass) {
+        return this.klass!= null && this.klass.getClassNumber()==klass.getClassNumber();
+    }
+
     @Override
     public String introduce(){
-        return ("My name is " + name + ". I am " + age + " years old. I am a " + OCCUPATION + ".");
+        return ("My name is " + name + ". I am " + age + " years old. I am a " + OCCUPATION + ". I am in class "+klass.getClassNumber()+".");
     }
+
 }
