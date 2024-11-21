043267dfcef4553a3f301707ca191d240a65c237
Emily Ng <ngem3@oocl.com>
2024-11-21 13:58:50 +0800

    feat: step1

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,30 @@
 package oo;
 
 public class Person {
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(int id, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String introduce(){
+        return ("My name is " + name + ". I am " + age + " years old.");
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Person anotherPerson = (Person) obj;
+        return this.id == anotherPerson.id;
+    }
+    @Override
+    public int hashCode() {
+        return this.id;
+    }
 }
